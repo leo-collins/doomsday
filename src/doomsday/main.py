34da093cd9f2097b0b7359e45023c9a61a57b591
Leo <leocollins511@gmail.com>
2025-04-08 21:23:19 +0100

    refactor: enhance doomsday_game function with click options for proleptic dates and repeat functionality

--- src/doomsday/main.py
+++ src/doomsday/main.py
@@ -2,20 +2,30 @@ from doomsday import generate_random_date, is_day_of_week
 import click
 
 @click.command()
-@click.option("--proleptic", default=False, help="Generate proleptic Gregorian dates.")
-@click.option("--repeat", default=False, help="Repeat the game indefinitely.")
+@click.option("--proleptic", is_flag=True, default=False, help="Generate proleptic Gregorian dates (dates before 1583).")
+@click.option("--repeat", is_flag=True, default=False, help="Repeat the game until the user decides to exit.")
 def doomsday_game(proleptic, repeat) -> None:
     """
     Play the Doomsday game. Generates a random date and asks the user to guess the day of the week.
     """
-    random_date = generate_random_date(proleptic=proleptic)
-    weekday_guess: str = click.prompt(f"Date: {random_date.strftime("%d %B %Y")} (Enter day of the week) ")
-    if weekday_guess == "exit":
-        click.echo("Exiting the game.")
-        return
-    if is_day_of_week(random_date, weekday_guess):
-        click.echo("Correct!")
-    else:
-        click.echo("Incorrect!")
-    click.echo(f"Date: {random_date.strftime('%d %B %Y')} is a {random_date.strftime("%A")}.")
+    i = 0
+    correct = 0
+    while True:
+        if repeat and i == 0:
+            click.echo("Repeating indefinitely. Type 'exit' to exit the game.")
+        random_date = generate_random_date(proleptic=proleptic)
+        weekday_guess: str = click.prompt(f"Date: {random_date.strftime("%d %B %Y")} (Enter day of the week)")
+        if weekday_guess == "exit":
+            click.echo(f"You got {correct} / {i + 1} correct.")
+            click.echo("Exiting the game.")
+            break
+        if is_day_of_week(random_date, weekday_guess):
+            click.echo(click.style("Correct! ", fg="green"), nl=False)
+            correct += 1
+        else:
+            click.echo(click.style("Incorrect! ", fg="red"), nl=False)
+        click.echo(f"{random_date.strftime('%d %B %Y')} is a {random_date.strftime("%A")}.")
+        if not repeat:
+            break
+        i += 1
 
