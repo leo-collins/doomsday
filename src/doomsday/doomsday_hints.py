03b712cc6f7b3804569f59c8b0db35f212212bdc
Leo <leocollins511@gmail.com>
2025-04-09 12:03:44 +0100

    reformatted layout of explanation text

--- src/doomsday/doomsday_hints.py
+++ src/doomsday/doomsday_hints.py
@@ -9,24 +9,19 @@ def anchor_day_century(date: date, verbose: bool = False) -> int:
     Args:
         date (date): `datetime.date` object representing the date.
 
-    Raises:
-        ValueError: If the date is before 1583. The Gregorian calendar was introduced in 1582.
-
     Returns:
         int: The anchor day for the century of the given date.
     """
-    if date.year < 1583:
-        raise ValueError("Date must be after 1582.")
     century = date.year // 100
     anchor_days = [1, 6, 4, 2]
     anchor_day = anchor_days[century % 4]
     if verbose:
-        print(f"Century: {century * 100}, Century anchor day: {days_reversed[anchor_day]}")
+        print("Step 1: Calculate the century anchor day")
+        print(f"Century: {century * 100}, Century anchor day: {days_reversed[anchor_day]} ({anchor_day})")
     return anchor_day
 
 def anchor_day_year(date: date, verbose: bool = False) -> int:
-    """Calculates the doomsday for the year of a given date.
-    Verbose mode prints the steps of this using the "odd+11" method.
+    """Calculates the doomsday for the year of a given date using the "odd + 11" method.
 
     Args:
         date (date): `datetime.date` representing the date.
@@ -37,30 +32,37 @@ def anchor_day_year(date: date, verbose: bool = False) -> int:
     """
     T = date.year % 100  # last two digits of the year
     if verbose:
-        print(f"Let T be the last two digits of the year, T = {T}")
+        print("")
+        print("Step 2: Calculate the doomsday for the year")
+        print(f"1) Let T be the last two digits of the year, T = {T}")
     # if T is odd, we add 11
     if T % 2 == 1:
         T += 11
         if verbose:
-            print(f"T is odd, so we add 11 to T to get T = {T}")
+            print(f"2) T is odd, so we add 11 to T to get T = {T}")
+    else:
+        if verbose:
+            print(f"2) T is even, so we don't add 11 to T")
     T //= 2  # divide T by 2
     if verbose:
-        print(f"Divide T by 2 to get T = {T}")
+        print(f"3) Divide T by 2 to get T = {T}")
     # if T is odd, we add 11
     if T % 2 == 1:
         T += 11
         if verbose:
-            print(f"T is odd, so we add 11 to T to get T = {T}")
-    T = 7 - (T % 7)
+            print(f"4) T is odd, so we add 11 to get T = {T}")
+    else:
+        if verbose:
+            print(f"4) T is even, so we don't add 11 to T")
+    T_mod7 = T % 7
+    T = 7 - T_mod7
     if verbose:
-        print(f"Subtract T mod 7 from 7 to get T = {T}")
+        print(f"5) We calculate 7 - (T mod 7) = 7 - {T_mod7} = {T}")
     # we count forward T days from the anchor day of the century
     century_anchor = anchor_day_century(date)
     doomsday = (century_anchor + T) % 7
     if verbose:
-        print(f"Add T to the anchor day of the century to get doomsday is {days_reversed[doomsday]}")
+        print(f"6) Add {T} to the anchor day of the century to get doomsday is {days_reversed[doomsday]} ({doomsday})")
     return doomsday
-
-
 
 
