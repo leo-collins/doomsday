fc57878b724a983620c28bf6d37cbcb3df1ffbd7
Leo <leocollins511@gmail.com>
2025-04-09 13:57:47 +0100

    feat: add calculate_day function and corresponding tests

--- src/doomsday/doomsday_hints.py
+++ src/doomsday/doomsday_hints.py
@@ -1,7 +1,37 @@
 from datetime import date
 from doomsday import days
+from calendar import isleap
 
 days_reversed: dict[int, str] = {val: key for key, val in days.items()}
+memorable_dates: dict[int, tuple[int, str]] = {
+    1: (3, "3rd 3 years in 4, 4th in the 4th"),
+    2: (28, "Last day of February"),
+    3: (14, "Pi day"),
+    4: (4, "4/4, 6/6, 8/8, 10/10, 12/12"),
+    5: (9, "Working 9-5 at 7-11"),
+    6: (6, "4/4, 6/6, 8/8, 10/10, 12/12"),
+    7: (11, "Working 9-5 at 7-11"),
+    8: (8, "4/4, 6/6, 8/8, 10/10, 12/12"),
+    9: (5, "Working 9-5 at 7-11"),
+    10: (10, "4/4, 6/6, 8/8, 10/10, 12/12"),
+    11: (7, "Working 9-5 at 7-11"),
+    12: (12, "4/4, 6/6, 8/8, 10/10, 12/12"),
+}
+months = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
 
 def anchor_day_century(date: date, verbose: bool = False) -> int:
     """Calculate the anchor day for the century of a given date.
@@ -66,3 +96,46 @@ def anchor_day_year(date: date, verbose: bool = False) -> int:
     return doomsday
 
 
+def calculate_day(date: date, verbose: bool = False) -> int:
+    """Calculates the day of the week for a given date using the Doomsday algorithm.
+
+    Args:
+        date (date): _description_
+        verbose (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        int: _description_
+    """
+    memorable_date, mnemonic = memorable_dates[date.month]
+    year_doomsday = anchor_day_year(date)
+    if isleap(date.year):
+        if date.month in [1, 2]:
+            memorable_date += 1
+    if verbose:
+        print(f"Step 3: Calculate the day")
+        print(f"Step 1) The doomsday for {months[date.month]} is {memorable_date} ({mnemonic}).")
+
+    if date.day > memorable_date:
+        # if the day is greater than the doomsday, we count forward
+        difference = date.day - memorable_date
+        day_of_week = (year_doomsday + difference) % 7
+        if verbose:
+            print(f"Step 2) The date is {difference} days after the doomsday.")
+            print(f"        We do doomsday + {difference} (mod 7) to get the day of the week.")
+            print(f"        The day of the week is therefore {year_doomsday} + {difference % 7} = {day_of_week}, which is a {days_reversed[day_of_week]}.")
+        return day_of_week
+    elif date.day < memorable_date:
+        # if the day is less than the doomsday, we count backward
+        difference = memorable_date - date.day
+        day_of_week = (year_doomsday - difference) % 7
+        if verbose:
+            print(f"Step 2) The date is {difference} days before the doomsday.")
+            print(f"        We do doomsday - {difference} (mod 7) to get the day of the week.")
+            print(f"        The day of the week is therefore {year_doomsday} - {difference % 7} = {day_of_week}, which is a {days_reversed[day_of_week]}.")
+        return day_of_week
+    else:
+        # if the day is equal to the doomsday, we return the doomsday
+        if verbose:
+            print(f"Step 2) The date is the same as the doomsday.")
+            print(f"        The day of the week is therefore {year_doomsday}, which is a {days_reversed[year_doomsday]}.")
+        return year_doomsday
--- tests/test_doomsday_hints.py
+++ tests/test_doomsday_hints.py
@@ -1,6 +1,6 @@
 import pytest
 from datetime import date
-from doomsday.doomsday_hints import anchor_day_century, anchor_day_year, days_reversed
+from doomsday.doomsday_hints import anchor_day_century, anchor_day_year, days_reversed, calculate_day
 
 @pytest.mark.parametrize(
     "year, expected_anchor_day",
@@ -42,3 +42,21 @@ def test_year_anchor_day(date_obj: date, doomsday: str):
     Test the anchor day for the year.
     """
     assert days_reversed[anchor_day_year(date_obj)] == doomsday
+
+@pytest.mark.parametrize(
+        "date_obj, expected_day",
+        [
+            (date(2025, 4, 9), "Wednesday"),
+            (date(2000, 9, 11), "Monday"),
+            (date(2024, 2, 29), "Thursday"),
+            (date(2000, 2, 29), "Tuesday"),
+            (date(1900, 2, 28), "Wednesday"),
+            (date(1900, 3, 1), "Thursday"),
+            (date(1985, 9, 18), "Wednesday"),
+        ]
+)
+def test_day(date_obj: date, expected_day: str):
+    """
+    Test the day of the week.
+    """
+    assert days_reversed[calculate_day(date_obj)] == expected_day
